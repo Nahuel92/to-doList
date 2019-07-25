89a6183d25337153a2826ad176165be27bf01124
nahuel.rodriguez <nahuel.rodriguez@mulesoft.com>
2019-07-25 10:02:43 -0300

    (feat): Some modifications were made to controller advice.

--- src/main/java/org/nahuelrodriguez/controllers/advices/TodoListControllerAdvice.java
+++ src/main/java/org/nahuelrodriguez/controllers/advices/TodoListControllerAdvice.java
@@ -1,7 +1,7 @@
 package org.nahuelrodriguez.controllers.advices;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.cassandra.CassandraConnectionFailureException;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class TodoListControllerAdvice {
-    @ExceptionHandler({DataAccessException.class})
-    public ResponseEntity<Object> handleException(DataAccessException exception) {
-        final String errorMessage = "Database access error. Error message:" + exception.getMessage();
-        return new ResponseEntity<>(errorMessage, null, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler({CassandraConnectionFailureException.class})
-    public ResponseEntity<Object> handleException() {
+    @ExceptionHandler({DataAccessResourceFailureException.class})
+    public ResponseEntity<Object> handleConnectionFailureException() {
         final String errorMessage = "Database connection failed. Please try again later.";
         return new ResponseEntity<>(errorMessage, null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({InvalidDataAccessApiUsageException.class})
+    public ResponseEntity<Object> handleInvalidQueryException() {
+        final String errorMessage = "An invalid query has been executed by the server.";
+        return new ResponseEntity<>(errorMessage, null, HttpStatus.BAD_REQUEST);
     }
 }
