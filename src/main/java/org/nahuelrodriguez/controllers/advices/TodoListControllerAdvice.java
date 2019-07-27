3a49b042c9d298da883466b4f91eff9212086f27
nahuel.rodriguez <nahuel.rodriguez@mulesoft.com>
2019-07-27 14:49:01 -0300

    (feat): Added more exception handlers.

--- src/main/java/org/nahuelrodriguez/controllers/advices/TodoListControllerAdvice.java
+++ src/main/java/org/nahuelrodriguez/controllers/advices/TodoListControllerAdvice.java
@@ -1,30 +1,34 @@
 package org.nahuelrodriguez.controllers.advices;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import org.nahuelrodriguez.responses.DTOErrors;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class TodoListControllerAdvice {
     @ExceptionHandler({DataAccessResourceFailureException.class})
-    public ResponseEntity<String> handleConnectionFailureException() {
-        final String errorMessage = "Database connection failed. Please try again later.";
-        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<DTOErrors<String>> handleConnectionFailureException() {
+        final DTOErrors<String> errors = new DTOErrors<>(List.of("Database connection failed. Please try again later."));
+        return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler({InvalidDataAccessApiUsageException.class})
-    public ResponseEntity<String> handleInvalidQueryException() {
-        final String errorMessage = "An invalid query has been executed by the server.";
-        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<DTOErrors<String>> handleInvalidQueryException() {
+        final DTOErrors<String> errors = new DTOErrors<>(List.of("An invalid query has been executed by the server."));
+        return new ResponseEntity<>(errors, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
@@ -34,7 +38,24 @@ public class TodoListControllerAdvice {
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
-
         return new ResponseEntity<>(new DTOErrors<>(errorMessages), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public ResponseEntity<DTOErrors<String>> handleMethodArgumentTypeMismatchException(HttpMessageNotReadableException exception) {
+        final DTOErrors<String> errors = new DTOErrors<>(List.of("Required request body is missing."));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
+    public ResponseEntity<DTOErrors<String>> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
+        final DTOErrors<String> errors = new DTOErrors<>(List.of(exception.getMessage() + ". Verify the URL and params and try again."));
+        return new ResponseEntity<>(errors, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler({MismatchedInputException.class, JsonParseException.class})
+    public ResponseEntity<DTOErrors<String>> handleMismatchedInputException() {
+        final DTOErrors<String> errors = new DTOErrors<>(List.of("Cannot deserialize data. Please check params and try again."));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
