b45c5c3e54d8854a685927304455a708a97ced6e
nahuel.rodriguez <nahuel.rodriguez@mulesoft.com>
2019-08-02 13:14:54 -0300

    (fix): Fixed Kafka producer topic.

--- src/main/java/org/nahuelrodriguez/services/implementation/KafkaProducer.java
+++ src/main/java/org/nahuelrodriguez/services/implementation/KafkaProducer.java
@@ -3,6 +3,7 @@ package org.nahuelrodriguez.services.implementation;
 import org.nahuelrodriguez.requests.dtos.TodoItemRequest;
 import org.nahuelrodriguez.services.MessagingQueueProducer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +12,16 @@ import java.util.Collection;
 @Service
 public class KafkaProducer implements MessagingQueueProducer {
     private final KafkaTemplate<String, TodoItemRequest> kafkaTemplate;
+    private final String topic;
 
     @Autowired
-    public KafkaProducer(final KafkaTemplate<String, TodoItemRequest> kafkaTemplate) {
+    public KafkaProducer(final KafkaTemplate<String, TodoItemRequest> kafkaTemplate,
+                         @Value("${spring.kafka.template.default-topic}") final String topic) {
         this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
     }
 
     public void sendMessage(final Collection<TodoItemRequest> dtos) {
-        dtos.forEach(dto -> this.kafkaTemplate.send("${spring.kafka.template.default-topic}", dto));
+        dtos.forEach(dto -> this.kafkaTemplate.send(topic, dto));
     }
 }
