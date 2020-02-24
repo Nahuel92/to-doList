cb8b552a5bae350f1bc9b61838cc712619121d5d
Nahuel Rodriguez <nahuel>
2020-02-24 19:15:15 -0300

    feat: Repository now extends from CassandraRepository instead of extending from CrudRepository.

--- src/main/java/org/nahuelrodriguez/daos/Repository.java
+++ src/main/java/org/nahuelrodriguez/daos/Repository.java
@@ -1,9 +1,9 @@
 package org.nahuelrodriguez.daos;
 
 import org.nahuelrodriguez.entities.TodoItem;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.cassandra.repository.CassandraRepository;
 
 import java.util.UUID;
 
-public interface Repository extends CrudRepository<TodoItem, UUID> {
+public interface Repository extends CassandraRepository<TodoItem, UUID> {
 }
